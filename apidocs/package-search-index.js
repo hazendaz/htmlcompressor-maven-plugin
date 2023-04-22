b3414bb0054a6fe9fcfd155f20ea64a25a161cb0
Jeremy Landis <jeremylandis@hotmail.com>
2023-04-22 19:13:13 -0400

    Site checkin for project Maven HTMLCompressor Plugin

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.hazendaz.maven.htmlcompressor_maven_plugin"},{"l":"com.tunyk.mvn.plugins.htmlcompressor"}];updateSearchResults();
