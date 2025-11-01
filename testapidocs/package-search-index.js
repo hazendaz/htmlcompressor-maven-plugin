ba28cc7c269a506a6ecda24f5f833112ba83d6f0
Jeremy Landis <jeremylandis@hotmail.com>
2025-11-01 18:15:10 -0400

    Site checkin for project htmlcompressor-maven-plugin

--- testapidocs/package-search-index.js
+++ testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.tunyk.mvn.plugins.htmlcompressor"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"com.tunyk.mvn.plugins.htmlcompressor"}];updateSearchResults();
